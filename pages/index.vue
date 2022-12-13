0c3e4453d69a17196878e90e8e5c4954e4008167
Justin Carruthers <Jtcruthers@gmail.com>
2022-12-13 15:03:04 -0500

    Added transition

--- pages/index.vue
+++ pages/index.vue
@@ -17,6 +17,28 @@ setInterval(() => {
 <template>
   <div class="w-full h-full">
     <!-- Add transition to text change -->
-      <h1 :key="counter" class="w-full h-full flex align-middle justify-center text-2xl m-auto text-center">{{ `Hello, ${name} :)` }}</h1>
+    <Transition name="fade" mode="out-in">
+      <h1 :key="counter" class="w-full h-full flex align-middle justify-center text-4xl m-auto text-center">{{ `Hello, ${name} :)` }}</h1>
+    </Transition>
   </div>
 </template>
+
+<style scoped>
+.fade-enter-active {
+  transition: opacity 250ms cubic-bezier(0.55, 0.085, 0.68, 0.53);
+}
+
+.fade-leave-active {
+  transition: opacity 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94);
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+h1 {
+  height: 100vh;
+  line-height: 100vh;
+}
+</style>
