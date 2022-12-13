99a67575a9f42ddcacd81286894f893fa14e94ae
Justin Carruthers <Jtcruthers@gmail.com>
2022-12-13 15:02:33 -0500

    Added pages

--- app.vue
+++ app.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="h-full w-full text-yellow-300 bg-red-700">
+    <NuxtPage />
   </div>
 </template>
+
+<style>
+html, body {
+  height: 100%;
+  width: 100%;
+}
+
+#__nuxt {
+  height: 100%;
+  width: 100%;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { computed } from "vue"
+const names = ["Justin", "Molly", "Ashe"]
+let counter = ref(0)
+
+const name = computed(() => {
+  const nameIndex = counter.value % names.length
+  return names[nameIndex]
+})
+
+setInterval(() => {
+  counter.value++
+}, 3500)
+
+</script>
+
+<template>
+  <div class="w-full h-full">
+    <!-- Add transition to text change -->
+      <h1 :key="counter" class="w-full h-full flex align-middle justify-center text-2xl m-auto text-center">{{ `Hello, ${name} :)` }}</h1>
+  </div>
+</template>
