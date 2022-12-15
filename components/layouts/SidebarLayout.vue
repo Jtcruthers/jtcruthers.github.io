7aff5fb5730547a08bae1bcfb9bfb96d79ea47a4
Justin Carruthers <Jtcruthers@gmail.com>
2022-12-14 22:17:23 -0500

    Added Ashe page

--- components/layouts/SidebarLayout.vue
+++ components/layouts/SidebarLayout.vue
@@ -78,9 +78,8 @@
       </div>
     </div>
     <div class="flex flex-1 flex-col md:pl-64 h-full">
-      <div class="sticky top-0 z-10 bg-gray-100 pl-1 pt-1 sm:pl-3 sm:pt-3 md:hidden">
+      <div class="sticky top-0 z-10 bg-red-400 pl-1 pt-1 sm:pl-3 sm:pt-3 md:hidden">
         <button type="button" class="-ml-0.5 -mt-0.5 inline-flex h-12 w-12 items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" @click="sidebarOpen = true">
-          <span class="sr-only">Open sidebar</span>
         </button>
       </div>
       <main class="flex-1">
---
+++ pages/ashe.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="mt-5 md:mt-8">
+    <div class="flex flex-row w-full items-center">
+      <img src="/images/ashe_profile.jpeg" alt="Ashe" class="inline-block h-36 w-36 sm:w-44 sm:h-44 md:h-52 md:w-52 rounded-full" />
+      <div class="w-full h-full text-center sm:text-right p-0 sm:pr-8 md:pr-12">
+        <h1 class="text-6xl w-full">Ashe</h1>
+        <span class="hidden lg:block text-white text-sm mr-1 mt-1">The best PupPup</span>
+      </div>
+    </div>
+
+    <Divider class="mt-8" />
+
+  </div>
+</template>
