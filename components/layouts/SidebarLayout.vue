0b0704b7b47ba41d01ffd188a67e9a2c9a5a3fc7
Justin Carruthers <Jtcruthers@gmail.com>
2022-12-15 02:14:11 -0500

    Dynamic router

--- components/layouts/SidebarLayout.vue
+++ components/layouts/SidebarLayout.vue
@@ -8,7 +8,7 @@
 
         <div class="fixed inset-0 z-40 flex">
           <TransitionChild as="template" enter="transition ease-in-out duration-300 transform" enter-from="-translate-x-full" enter-to="translate-x-0" leave="transition ease-in-out duration-300 transform" leave-from="translate-x-0" leave-to="-translate-x-full">
-            <DialogPanel class="relative flex w-full max-w-xs flex-1 flex-col bg-gray-800">
+            <DialogPanel class="relative flex w-full max-w-xs flex-1 flex-col bg-red-400">
               <TransitionChild as="template" enter="ease-in-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in-out duration-300" leave-from="opacity-100" leave-to="opacity-0">
                 <div class="absolute top-0 right-0 -mr-12 pt-2">
                   <button type="button" class="ml-1 flex h-10 w-10 items-center justify-center rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" @click="sidebarOpen = false">
@@ -21,23 +21,10 @@
                   <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
                 </div>
                 <nav class="mt-5 space-y-1 px-2">
-                  <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-800 hover:bg-red-500 hover:text-white', 'group flex items-center px-2 py-2 text-base font-medium rounded-md']">
-                    {{ item.name }}
-                  </a>
+                  <span v-for="item in navigation" :key="item.name" :class="[item.current ? 'bg-red-700 text-red-50' : 'text-gray-100 hover:bg-red-400 hover:text-white', 'group flex items-center px-2 py-2 text-base font-medium rounded-md']">
+                    <NuxtLink :to="item.to" @click="sidebarOpen = false">{{ item.name }}</NuxtLink>
+                  </span>
                 </nav>
-              </div>
-              <div class="flex flex-shrink-0 bg-gray-700 p-4">
-                <a href="#" class="group block flex-shrink-0">
-                  <div class="flex items-center">
-                    <div>
-                      <img class="inline-block h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-                    </div>
-                    <div class="ml-3">
-                      <p class="text-base font-medium text-white">Tom Cook</p>
-                      <p class="text-sm font-medium text-gray-400 group-hover:text-gray-300">View profile</p>
-                    </div>
-                  </div>
-                </a>
               </div>
             </DialogPanel>
           </TransitionChild>
@@ -49,7 +36,7 @@
     </TransitionRoot>
 
     <!-- Static sidebar for desktop -->
-    <div class="hidden md:fixed md:inset-y-0 md:flex md:w-64 md:flex-col">
+    <div class="hidden md:fixed md:inset-y-0 md:flex md:w-52 md:flex-col">
       <!-- Sidebar component, swap this element with another sidebar if you like -->
       <div class="flex min-h-0 flex-1 flex-col bg-red-400">
         <div class="flex flex-1 flex-col overflow-y-auto pt-5 pb-4">
@@ -57,29 +44,17 @@
             <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
           </div>
           <nav class="mt-5 flex-1 space-y-1 px-2">
-            <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-red-600 text-red-50' : 'text-gray-100 hover:bg-red-500 hover:text-white', 'group flex items-center px-2 py-2 text-sm font-medium rounded-md']">
-              {{ item.name }}
-            </a>
+            <span v-for="item in navigation" :key="item.name" :class="[item.current ? 'bg-red-600 text-red-50' : 'text-gray-100 hover:bg-red-500 hover:text-white', 'group flex items-center px-2 py-2 text-sm font-medium rounded-md']">
+              <NuxtLink :to="item.to">{{ item.name }}</NuxtLink>
+            </span>
           </nav>
-        </div>
-        <div class="flex flex-shrink-0 bg-gray-700 p-4">
-          <a href="#" class="group block w-full flex-shrink-0">
-            <div class="flex items-center">
-              <div>
-                <img class="inline-block h-9 w-9 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-              </div>
-              <div class="ml-3">
-                <p class="text-sm font-medium text-white">Tom Cook</p>
-                <p class="text-xs font-medium text-gray-300 group-hover:text-gray-200">View profile</p>
-              </div>
-            </div>
-          </a>
         </div>
       </div>
     </div>
     <div class="flex flex-1 flex-col md:pl-64 h-full">
       <div class="sticky top-0 z-10 bg-red-400 pl-1 pt-1 sm:pl-3 sm:pt-3 md:hidden">
         <button type="button" class="-ml-0.5 -mt-0.5 inline-flex h-12 w-12 items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500" @click="sidebarOpen = true">
+          <Bars3Icon class="h-6 w-6 text-red-800" aria-hidden="true" />
         </button>
       </div>
       <main class="flex-1">
@@ -91,14 +66,17 @@
   </div>
 </template>
 
-<script setup>
-import { ref } from 'vue'
+<script setup lang="ts">
+import { ref, computed } from 'vue'
 import { Dialog, DialogPanel, TransitionChild, TransitionRoot } from '@headlessui/vue'
+import { Bars3Icon } from '@heroicons/vue/20/solid'
 
-const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'About', href: '#', current: false },
-]
+const route = useRoute()
+
+const navigation = computed(() => [
+  { name: 'Home', to: '/', current: route.path === '/'},
+  { name: 'Ashe', to: '/ashe', current: route.path === '/ashe' },
+])
 
 const sidebarOpen = ref(false)
 </script>
