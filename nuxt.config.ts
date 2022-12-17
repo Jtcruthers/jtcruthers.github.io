b3c1e24f0de3a7bcc97d5cb5362508caf7ee3c4b
Justin Carruthers <Jtcruthers@gmail.com>
2022-12-16 23:32:14 -0500

    gh pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
+  router: {
+    base: '/jtcruthers.github.io/'
+  },
   modules: [
     '@nuxtjs/tailwindcss'
   ]
