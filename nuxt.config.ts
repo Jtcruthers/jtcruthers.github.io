9347a7182f7dfddd7ad9149654a04f2257a8c2ad
Justin Carruthers <Jtcruthers@gmail.com>
2023-09-02 14:02:22 -0400

    attempt to deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
+  baseURL: /jtcruthers.github.io/,
   srcDir: 'src',
   router: {
     base: '/jtcruthers.github.io/'
