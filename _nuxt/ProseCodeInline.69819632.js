840cd49f6d4bb02e09427e624cb5fa930b5174a6
Justin Carruthers <Jtcruthers@gmail.com>
2023-09-05 19:11:11 -0400

    Updates

---
+++ _nuxt/ProseCodeInline.69819632.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./_plugin-vue_export-helper.c27b6911.js";import{o as n,c as s,E as c}from"./entry.08586ea1.js";const t={},_={class:"prose-code-inline"};function r(e,d){return n(),s("code",_,[c(e.$slots,"default",{},void 0,!0)])}const l=o(t,[["render",r],["__scopeId","data-v-4dcffdac"]]);export{l as default};
