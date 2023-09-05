7788ac96b1250a47556b20e98c69b3ddc12b9500
Justin Carruthers <Jtcruthers@gmail.com>
2023-09-05 19:09:03 -0400

    baseurl

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   ssr: false,
   app: {
-    baseURL: process.env.NUXT_APP_BASE_URL || '/jtcruthers.github.io/'
+    baseURL: process.env.NUXT_APP_BASE_URL || '/'
   },
   srcDir: 'src',
   modules: [
